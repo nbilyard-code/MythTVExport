979a916c63a4d4a9ab559742fe6ed0093c013940
nbilyard-code <61681387+nbilyard-code@users.noreply.github.com>
2020-10-14 10:27:51 -0700

    Update mkv_export.sh

--- mkv_export.sh
+++ mkv_export.sh
@@ -2,21 +2,20 @@
 #
 # This bash script is for exporting all your mythtv recording as h264 mkv files to a new directory,
 # and renaming the new encoded files in a human readable format.  This script has been tested on mythtv v31.
-# The orginal files were recoreded with a InfiniTV PCI using a cablecard to decode.
-# The original files are a mix of mpg and ts files.
+# The tested files were recoreded with a InfiniTV PCI using a cablecard to decode, and were a mix of mpg and ts files.
 #
 # The script will query the mythtv database, and pull out the name of the show, the show title, season, and episode
 # number.  It will use the file name to run a mysql SELECT query on the mythconverg database recorded table. The script
 # will NOT change your database nor will it delete or modify the original recording.  The script assumes that the mythtv recordings
 # are in the default naming format of xxxx_yyyymmddssmmmm.extension (chanid_starttimeUTF)
 #
-# The script will use HandBrakeCLI to encode the video, and will keep looking though all the files until complete.
+# The script will use HandBrakeCLI to encode the video, and will keep looping though all the files until complete.
 # This script is meant to loop through all the files in the recoreded directory and transcode all files that end in .mpg or .ts.
 # It will check to see if a show has been exported first, and will not re-export if it already exists in the export directory.
 # You can change the number of threads to use by editing the threads statement in the Handbrake command.  It is currently
 # set to use 5 threads.
 #
-# To use this script, you can call it directly.  It takes no arguments from the command line.
+# To use this script, you call it directly.  It takes no arguments from the command line.
 # You will need to install handbrake-cli inorder to use.  (e.g. sudo apt install handbrake-cli)
 #
 #
@@ -40,6 +39,7 @@ db='mythconverg'
 cd $RECORD_DIR
 for file in *.mpg *.ts
 	do
+	# Get Database info on the file
 	title=$(mysql -u $user -h $host -D $db -se "SELECT title FROM recorded WHERE basename='$file'")
 	subtitle=$(mysql -u $user -h $host -D $db -se "SELECT subtitle FROM recorded WHERE basename='$file'")
 	season=$(mysql -u $user -h $host -D $db -se "select season FROM recorded WHERE basename='$file'")
